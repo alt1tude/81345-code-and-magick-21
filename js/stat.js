2a1176751cb818315da85b3424de3ddd6c5e8b2a
alt1tude <fort1tude@mail.ru>
2020-09-29 14:20:16 +0700

    Наработк и по уч.проекту

---
+++ js/stat.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const renderStatistics = function (ctx, names, times) {
+
+
+};
+
+
+
+  // var fireballSize = 22;
+
+  // var getFireballSpeed = function (isMovingleft) {
+  //   return isMovingleft ? 2 : 5;
+  // }
+
+  // var wizardWidth = 70;
+
+  // var getWizardHeight = function () {
+  //   return 1.337*wizardWidth;
+  // }
+
+  // var wizardSpeed = 3;
+
+  // var getWizardX = function (gameFieldWidth) {
+  //   return (gameFieldWidth - wizardWidth)/2;
+  // }
+
+  // var getWizardY = function (gameFieldHeight) {
+  //   return gameFieldHeight/3;
+
+
